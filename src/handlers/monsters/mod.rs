17a8de0434ff2a4676f2c58002b904de2946c5ab
echooferen <edygithub@outlook.com>
2024-05-26 11:01:07 -0400

    feat: rename delete_monster_by_id and update_monster_by_id handlers

--- src/handlers/monsters/mod.rs
+++ src/handlers/monsters/mod.rs
@@ -1,6 +1,6 @@
 pub mod create_monster;
-pub mod delete_monster;
+pub mod delete_monster_by_id;
 pub mod get_monster_by_id;
 pub mod get_monsters;
 pub mod import_csv;
-pub mod update_monster;
+pub mod update_monster_by_id;
